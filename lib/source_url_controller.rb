0c121bfb736ad1df1fc6bf1956f3892fbdab43dd
motoki3410 <windhole922@gmail.com>
2021-06-28 17:23:58 +0900

    Modified add comment

--- lib/source_url_controller.rb
+++ lib/source_url_controller.rb
@@ -5,13 +5,32 @@ class SourceURLController
 
     def add (url,register_name)
         #add url to registere.json
+
+        #(1) open register.json
+        #(2) push register_name
+        #(2-1) error (sting)
+        #(3) push url
+        #(3-1) error (string)
+        #(4) close register.json
     end
 
     def delete (url,register_name)
         #delete url of registere.json
+
+        #(1) open register.json
+        #(2) delete register_name
+        #(3) delete url
+        #(4) close register.json
+
     end
 
     def list ()
         #read register.json
+
+        #(1) oepn register.json
+        #(2) create data
+            # [{"url"=>"http//...","register_name"=>"文書管理システム"}, 
+            # {}...*10]
+        #(3)return (2)
     end
 end
